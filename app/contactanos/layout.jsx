303538d854ae954b85c469228ffeb89359a40657
Usiel-03A7 <shebastyan@gmail.com>
2023-10-01 09:05:05 -0600

    jugando con los fonts en next

--- app/contactanos/layout.jsx
+++ app/contactanos/layout.jsx
@@ -1,11 +1,16 @@
-import Navbar from "./page"
+import {Roboto} from "next/font/google"
 export const metadata = {
   title:"Contactanos "
 }
+const roboto = Roboto({
+  weight:["300","400","700"],
+  style:["italic", "normal"],
+  subsets: ["latin-ext"]
+})
 export default function Layout({children}){
   return (
-    <>
+    <div className={roboto.className}>
     {children}
-    </>
+    </div>
   )
 }
